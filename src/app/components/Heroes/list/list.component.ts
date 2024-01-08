61a0a7435b226bcdca0baa5116014804f329f2ff
Jonathan <djgrovezone@gmail.com>
2024-01-08 20:25:57 +0100

    Adding HTML and TS code with input and output

--- src/app/components/Heroes/list/list.component.ts
+++ src/app/components/Heroes/list/list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Hero, UniverseEnum } from '../hero/hero.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  universe = UniverseEnum;
+  @Input() heroes!: Hero[];
+  @Output() selectedHero = new EventEmitter<Hero>();
 
+  selectHero(value: Hero): void {
+    this.selectedHero.emit(value);
+  }
 }
